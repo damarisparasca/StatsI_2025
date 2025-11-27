f3cf0e2dee3bd2e95c4b152e73e890848649ba93
Damaris Parasca <126685870+damarisparasca@users.noreply.github.com>
2025-11-27 17:50:55 +0000

    ps4

--- problemSets/PS04/my_answers/PS04_DP.R
+++ problemSets/PS04/my_answers/PS04_DP.R
@@ -37,12 +37,11 @@ help(Prestige)
 
 # Question 1
 df = Prestige
-
 df$professional <- ifelse(df$type == "prof", 1, 0)
 df
 
 pres_model = lm(prestige ~ income + professional +
-				prestige:professional, data = df)
+				income:professional, data = df)
 pres_model
 stargazer(pres_model)
 
@@ -55,29 +54,32 @@ me_income
 prof_yes = predict(pres_model,newdata=data.frame(professional = 1,income = 6000))
 prof_no = predict(pres_model,newdata=data.frame(professional = 0,income = 6000))
 
-me_prof = prof_no - prof_yes
+me_prof = prof_yes - prof_no
 me_prof
 
 # Question 2 
 # h0: beta = 0
 # hA: beta not = 0
 
+# test statistic
 t_stat1 = 0.042/0.016
 t_stat1
 
-deg_fr = 30 + 76 - 2 - 1
+# degrees of freedom
+deg_fr = 131 - 2 - 1
 
+# calculate the p-value
 p_value1 = 2*pt(t_stat1, deg_fr, lower.tail = F)
 p_value1
 
 # h0: beta = 0
 # hA: beta not = 0
 
+# test statistic
 t_stat2 = 0.042/0.013
 t_stat2
 
-deg_fr = 131 - 2 - 1
-
+# calculate the p-value
 p_value2 = 2*pt(t_stat2, deg_fr, lower.tail = F)
 p_value2
 
